5c6415e9ad403bf036b01e239a2a2400e28ada6f
toddr <todd.romkema@gmail.com>
2019-11-03 20:01:53 -0600

    Added call valuation

--- src/com/company/Binomial.java
+++ src/com/company/Binomial.java
@@ -11,7 +11,7 @@ public class Binomial {
          */
 
         double diff = a - b;
-        if ( (diff * a * b <= 0.0000000000003) && (diff * a * b >= -0.0000000000003)) {
+        if ( Math.abs(diff)  <= ( ((a + b)/2) * 0.0000000000003) ) {
             return true;
         }
         else {
@@ -58,7 +58,7 @@ public class Binomial {
         try {
             p =  (r - d + 1) / (u -d);
             if (compEquals(r, (p*profitUp + (1-p)*profitDown))) {
-                return round(p,15);
+                return round(p,13);
             }
             else {
                 throw new Exception("A problem has occured in processing the calcProbUp inputs + \n"
@@ -74,5 +74,54 @@ public class Binomial {
         return p;
     }
 
+    public static double afterOnePeriodCallValuation(double u, double d, double r, double S, double X) {
+        /*
+        The extra values here that are not found in calcProbUp are S, the current asset price, and c, the call value
+        of an option.
+        The goal of this function is to find the expected value, and therefore the risk-neutral valuation, of an option
+        with exercise price X on an asset with current value S after one period in which the asset either appreciates
+        to a value of uS or depreciates to a value of dS.
+         */
+
+        double p = calcProbUp(u, d, r);
+        double cPlus = Math.max(u*S - X, 0); // cPlus is the exercise value if the asset increases in value.
+        double cMinus = Math.max(d*S - X, 0); // cMinus is the exercise value of the asset decreases in value, often 0.
+        return round(p*cPlus + (1-p) * cMinus, 13);
+    }
+
+    public static double currentValueOnePeriodCall(double u, double d, double r, double S, double X) {
+        /*
+        Returns the current value of a one-period European call option, as opposed to the expected value of its value
+        at the exercise date (which is what afterOnePeriodCallValuation returns).
+         */
+        double valueTOne = afterOnePeriodCallValuation(u, d, r, S, X);
+        return round((valueTOne / (1 + r)), 13);
+    }
+
+    public static double afterTwoPeriodCallValuation (double u, double d, double r, double S, double X) {
+        /*
+        The goal of this function is to find the expected value, and therefore the risk-neutral valuation, of an option
+        with exercise price X on an asset with current value S after TWO periods.
+         */
+
+        double p = calcProbUp(u, d, r);
+        double cPlusPlus = Math.max(u*u*S - X, 0);
+        // cPlusPlus is the exercise value if the asset increases in value twice.
+        double cPlusMinus = Math.max(u*d*S - X, 0);
+        // cPlusMinus is the exercise value of the asset increases in value once and decreases in value once.
+        double cMinusMinus = Math.max(d*d*S - X, 0);
+        // cMinusMinus is the exercise value if the asset decreases in value twice, often zero.
+        return round(p*p*cPlusPlus + p*(1-p)*2*(cPlusMinus) + (1-p)*(1-p)*cMinusMinus,13);
+    }
+
+    public static double currentValueTwoPeriodCall (double u, double d, double r, double S, double X) {
+        /*
+        Returns the current value of a European call option with an exercise date after two periods,
+        where each period the asset either increases in value to a multiple of u or decreases in value to a multiple
+        of d.
+         */
+        double valueTTwo = afterTwoPeriodCallValuation(u, d, r, S, X);
+        return round((valueTTwo / ((1+r)*(1+r)) ), 10);
+    }
 
 }
